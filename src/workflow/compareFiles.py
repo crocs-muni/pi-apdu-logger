b7d2f488991899a7f2b01865072152e6821c1932
Lejla125 <lejla.dzaferbegovic@gmail.com>
2023-03-09 22:30:41 +0100

    Refactored file comparison script

--- src/workflow/compareFiles.py
+++ src/workflow/compareFiles.py
@@ -5,43 +5,37 @@ from collections import Counter
 """
 This function compares the hex dump from gp.exe and hex dump captured by RaspberryPi.
 For more info about Global Platform see https://github.com/martinpaljak/GlobalPlatformPro
+It saves the comparison log to a textual file and calculates the error rate.
 """
 
-def calculate_offset(words :list) -> int:
-    offset = 6
+def calculate_offset(words: list) -> int:
+    offset = 6 # first 3 bytes (6 chars) form the prologue field of the block frame for transmission protocol T=1
     for word in words:
         offset += len(word)
     
     return offset
 
-def convert_to_list(stream :string, offset :int) -> list:
+def convert_to_list(stream: string, offset: int) -> list:
     byte_list = []
     for i in range(0, len(stream), 2):
         byte_list.append(stream[offset+i:offset+i+2])
     
     return byte_list
 
-def compare_byte_by_byte(expected, captured, offset) -> list:
-    differences = []
-    expected_bytes = convert_to_list(expected, 0)
-    captured_bytes = convert_to_list(captured, offset)
+def compare_byte_by_byte(expected_stream: string, captured_stream: string, offset: int) -> list:
+    difference = []
+    expected_bytes = convert_to_list(expected_stream, 0)
+    captured_bytes = convert_to_list(captured_stream, offset)
     
-    # for i in range(0, len(expected), 2):
-    #     expected_byte = expected[i:i+2]
-    #     captured_byte = captured[offset+i:offset+2+i]
-
-    #     if(expected_byte != captured_byte):
-    #         pair = (expected_byte, captured_byte)
-    #         differences.append(pair)
-
-    count = Counter(expected_bytes) # count items in a
-    count.subtract(captured_bytes)  # subtract items that are in b
+    count = Counter(expected_bytes) # count items in expected_bytes list
+    count.subtract(captured_bytes)  # subtract items that are in the captured_bytes list
+    
     for byte in expected_bytes:
         if count[byte] > 0:
            count[byte] -= 1
-           differences.append(byte)
+           difference.append(byte)
     
-    return differences
+    return difference
 
 def compare_files(in_file1_path: string, in_file2_path: string, out_file_path: string):
     expected_in = open(in_file1_path,'r')
@@ -52,41 +46,37 @@ def compare_files(in_file1_path: string, in_file2_path: string, out_file_path: s
     captured_lines = captured_in.readlines()
     
     if(len(expected_lines) != len(captured_lines)):
-        comparison_result.write("Files have different lengths!")
+        comparison_result.write("Files have different lengths! Cannot compare them.")
         sys.exit()
 
     total_bytes = 0
     missing_bytes = 0
 
     for i in range(len(expected_lines)):
-        exp_words = expected_lines[i].split() # expected hexwords from the template file
-        search_start_index = 0
-        search_end_index = len(captured_lines[i])
+        expected_words = expected_lines[i].split() # expected hexwords from the template file
+        start_index = 0
+        end_index = len(captured_lines[i])
         
-        for j in range(len(exp_words)):
-            total_bytes += len(exp_words[j])
-            found_word_index = captured_lines[i].upper().find(exp_words[j], search_start_index, search_end_index)
+        for j in range(len(expected_words)):
+            total_bytes += len(expected_words[j])
+            found_at_index = captured_lines[i].upper().find(expected_words[j], start_index, end_index)
             
-            if(found_word_index == -1): # the word was not found by the find() method
+            if(found_at_index == -1): # the word was not found by the find() method
                 if i == 0:
-                    offset = 0
+                    offset = 0 # first line consists of the ATR
                 else: 
-                    offset = calculate_offset(exp_words[0:j])
+                    offset = calculate_offset(expected_words[0:j])
                 
-                diff = compare_byte_by_byte(exp_words[j], captured_lines[i].upper(), offset)
+                diff = compare_byte_by_byte(expected_words[j], captured_lines[i].upper(), offset)
                 missing_bytes += len(diff)
                 
-                comparison_result.write(f"\n{len(diff)} byte(s) in word " + exp_words[j] + " not correct.")
-
-                for elem in diff:
-                       comparison_result.write(f"\n-missing byte: {elem}") 
-                
-                # for pair in diff:
-                #     comparison_result.write(f"\nexpected: {pair[0]}, captured: {pair[1]}")
+                comparison_result.write(f"\n{len(diff)} byte(s) in word " + expected_words[j] + " not correct.")
+                for byte in diff:
+                       comparison_result.write(f"\n-missing byte: {byte}") 
                 
             else:
-                comparison_result.write("\nWord: " + exp_words[j] + " found in line " + str(i+1) + " at index " + str(found_word_index) + ".")
-                search_start_index = found_word_index + len(exp_words[j]) # move the search start index after the found word
+                comparison_result.write("\nWord: " + expected_words[j] + " found in line " + str(i+1) + " at index " + str(found_at_index) + ".")
+                start_index = found_at_index + len(expected_words[j]) # move the search start index after the found word
         
         comparison_result.write("\n------------------------------------------------------")
     
