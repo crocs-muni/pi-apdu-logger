fd9c9f22723612a07eab547d80c2383286a492ae
Lejla125 <lejla.dzaferbegovic@gmail.com>
2023-12-11 20:41:36 +0100

    fix indentation

--- src/helpers/counter.cpp
+++ src/helpers/counter.cpp
@@ -8,21 +8,21 @@
 using namespace std;
 
 /* 
-    This is a helper module for the APDULogger
-    It prints a histogram of possible sample sizes for the Filter module
+	This is a helper module for the APDULogger
+	It prints a histogram of possible sample sizes for the Filter module
 */
 
 int main(int argc, char **argv) 
 {
 	char filename[20];
-  ifstream indata;
-    
+	ifstream indata;
+
 	cout << "Enter raw data file name: ";
 	cin >> filename;
-    
+
 	cout << "Opening file " << filename << "..." << endl;
-	
-  indata.open(filename);
+
+	indata.open(filename);
 	if(!indata)
 	{
 		cerr << "Error: file to read from could not be opened!" << endl;
@@ -34,23 +34,23 @@ int main(int argc, char **argv)
 	int sample = 0;
 	vector<int> frequencies;
 	vector<int> histogram(101, 0);
-	
-	
-  cout << "Computing histogram... " << endl;
-  while(!indata.eof()) 
+
+
+	cout << "Computing histogram... " << endl;
+	while(!indata.eof()) 
 	{
 		indata.get(current_bit);
 		sample++;
 		if(indata.eof()) break;
 
-    if(indata.peek() != current_bit)
+		if(indata.peek() != current_bit)
 		{
 			frequencies.push_back(sample); //push back the current frequency and reset the counter
 			sample = 0;
-        }
+		}
 	}
 	indata.close();
-	
+
 	for (auto it = frequencies.begin(); it != frequencies.end(); it++)
 	{
 		if(*it <= 100) histogram[*it]++; //compute a histogram of possible sample sizes
@@ -61,6 +61,6 @@ int main(int argc, char **argv)
 	{
 		cout << "Sample size " << i << " observed " << histogram[i] << " time(s)." <<endl;
 	}
-    
+
 	return 0;
 }
